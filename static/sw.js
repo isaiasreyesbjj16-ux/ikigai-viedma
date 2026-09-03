2367063bf37ebda28b6b834e0f216f6a7949421c
Isaias Reyes Arzamendia <isaiasreyesbjj16@gmail.com>
2026-09-03 20:00:02 -0300

    actualizacion

--- static/sw.js
+++ static/sw.js
@@ -1,11 +1,18 @@
 /* Service worker - Academia */
+const CACHE_NAME = 'ikigai-static-v2';
+
 self.addEventListener('install', (e) => {
   self.skipWaiting();
-  e.waitUntil(caches.open('ikigai-static').then((c) => c.add('/').catch(() => {})));
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.add('/').catch(() => {})));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil((async () => {
+    // borrar cachés viejas para forzar actualizacion del SW en los dispositivos
+    const keys = await caches.keys();
+    await Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
 
 self.addEventListener('fetch', (e) => {
@@ -19,7 +26,7 @@ self.addEventListener('fetch', (e) => {
     fetch(req).then((res) => {
       const copy = res.clone();
       if (res.ok && url.pathname.startsWith('/static/')) {
-        caches.open('ikigai-static').then((c) => c.put(req, copy));
+        caches.open(CACHE_NAME).then((c) => c.put(req, copy));
       }
       return res;
     }).catch(() =>
